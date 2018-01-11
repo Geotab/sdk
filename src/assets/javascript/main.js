c79adb4fa67f05db6b423449dca4f33e90ac6346
Steve Hansen <stevehansen@geotab.com>
2018-01-11 16:18:17 -0500

    Fix sidebar toggle for small screens

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -2,7 +2,7 @@ var anchorForId = function (id) {
     var anchor = document.createElement("a");
     anchor.className = "header-link";
     anchor.href = "#" + id;
-    anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>';
+    anchor.innerHTML = `<span class="sr-only">Permalink</span><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`;
     anchor.title = "Permalink";
     return anchor;
 };
@@ -19,7 +19,21 @@ var linkifyAnchors = function (level, containingElement) {
     }
 };
 
-window.addEventListener('DOMContentLoaded', () => {
+var setupSidebarToggle = function () {
+    const toggleClass = " active";
+    var taggler = document.querySelector("#toggler");
+    var wrapper = document.querySelector("#wrapper");
+    taggler.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (wrapper.className.indexOf(toggleClass) > -1) {
+            wrapper.className = wrapper.className.replace(toggleClass, "");
+        } else {
+            wrapper.className += toggleClass;
+        }
+    });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
     var contentBlock = document.querySelector("article");
     if (!contentBlock) {
         return;
@@ -27,13 +41,8 @@ window.addEventListener('DOMContentLoaded', () => {
     for (var level = 2; level <= 3; level++) {
         linkifyAnchors(level, contentBlock);
     }
+    setupSidebarToggle();
 });
-
-document.onreadystatechange = function () {
-    if (this.readyState === "complete") {
-
-    }
-};
 
 (function (document, history, location) {
     var HISTORY_SUPPORT = !!(history && history.pushState);
@@ -47,8 +56,8 @@ document.onreadystatechange = function () {
          */
         init: function () {
             this.scrollToCurrent();
-            window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
-            document.body.addEventListener('click', this.delegateAnchors.bind(this));
+            window.addEventListener("hashchange", this.scrollToCurrent.bind(this));
+            document.body.addEventListener("click", this.delegateAnchors.bind(this));
         },
 
         /**
@@ -102,8 +111,8 @@ document.onreadystatechange = function () {
             var elem = e.target;
 
             if (
-                elem.nodeName === 'A' &&
-                this.scrollIfAnchor(elem.getAttribute('href'), true)
+                elem.nodeName === "A" &&
+                this.scrollIfAnchor(elem.getAttribute("href"), true)
             ) {
                 e.preventDefault();
             }
@@ -111,6 +120,6 @@ document.onreadystatechange = function () {
     };
 
     window.addEventListener(
-        'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
+        "DOMContentLoaded", anchorScrolls.init.bind(anchorScrolls)
     );
 })(window.document, window.history, window.location);
