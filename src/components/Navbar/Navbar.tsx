ee5c47ff5b0c52d8a69e1bb604f03443a8c0fefa
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2023-10-12 00:11:12 +0200

    INTGR-142: feat/initial side nav implementation  - Add Geotab SDK site logo  - Add references to SideNavigation components, whatever is    available so far

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,35 +1,43 @@
-
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { MenuContext } from '../../menuContext';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { MenuContext } from "../../menuContext";
+import {
+  SideNavigationBase,
+  SideNavigationCollapseProvider,
+} from "@geotab/react-component-library";
+import { LogoGeotabSDK } from "../Logo/LogoGeotabSDK";
 
 export default function Navbar() {
-
   const section = useContext(MenuContext);
 
   return (
     <div>
-      <div>
-        <Link to="/myGeotabIntroduction">Introduction</Link>
-      </div>
-      <div>
-        <Link to="/whatsNew">What's New</Link>
-      </div>
-      <div>
-        Guides
-        <div>
-          <Link to="/gettingStarted">Getting Started</Link>
-        </div>
-      </div>
-      <div>
-        API Reference
-        <div>
-          <Link to="/methods">Methods</Link>
-        </div>
-        <div>
-          <Link to="/objects">Objects</Link>
-        </div>
-      </div>
+      <SideNavigationCollapseProvider>
+        <SideNavigationBase>
+          <SideNavigationBase.Main Logo={LogoGeotabSDK} />
+          <div>
+            <Link to="/myGeotabIntroduction">Introduction</Link>
+          </div>
+          <div>
+            <Link to="/whatsNew">What's New</Link>
+          </div>
+          <div>
+            Guides
+            <div>
+              <Link to="/gettingStarted">Getting Started</Link>
+            </div>
+          </div>
+          <div>
+            API Reference
+            <div>
+              <Link to="/methods">Methods</Link>
+            </div>
+            <div>
+              <Link to="/objects">Objects</Link>
+            </div>
+          </div>
+        </SideNavigationBase>
+      </SideNavigationCollapseProvider>
     </div>
   );
 }
