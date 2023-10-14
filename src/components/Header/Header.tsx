98c29200d289381fd36a29588616ec4df929eda9
williamwgeotab <yaopanwong@geotab.com>
2023-10-14 01:30:46 -0400

    INTGR-141: feat/added search modal and logic for active link - added temp search modal for search bar - removed search bar component with temp search element - removed initial set up for Tab component - add Link component & testing logic for setting active link - add more styling for toolbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,32 +2,80 @@ import React, { useState } from "react";
 import { MenuContext } from "../../menuContext";
 import { LogoGeotabSDK } from "../Logo/LogoGeotabSDK";
 import { Link } from "react-router-dom";
-import SearchBar from "../../components/Searchbar/Searchbar";
+import SearchModal from "../../components/Header/SearchModal";
 import "./header.scss";
 
 export default function Header(props: any) {
-  const [active, setActive] = useState("myGeotab");
+  const [active, setActive] = useState("");
+  const [isSearchModalOpen, setSearchModalOpen] = useState(false);
+
+  const openSearchModal = () => {
+    setSearchModalOpen(true);
+  };
+
+  const closeSearchModal = () => {
+    setSearchModalOpen(false);
+  };
+
+  const handleLinkClick = (target: string) => {
+    setActive(target);
+    console.log("Active state is now:", target);
+  };
+
+  const isActive = (target: string) => {
+    const isActiveClass = target === active ? "active-button" : "";
+    console.log("isActive for", target, "is", isActiveClass); // Add this line
+    return isActiveClass;
+  };
 
   return (
     <MenuContext.Provider value={active}>
       <header className="header-container">
         {props.isLandingPage && <LogoGeotabSDK className="geotab-sdk-logo" />}
         <div className="tabs-container">
-          <div className="tab-item">
-            <Link to="/myGeotabIntroduction">myGeotab</Link>
+          <div className={`tab-item ${isActive("myGeotab")}`}>
+            <Link
+              to="/myGeotabIntroduction"
+              onClick={() => handleLinkClick("myGeotab")}
+            >
+              myGeotab
+            </Link>
           </div>
-          <div className="tab-item">
-            <Link to="/myAdminIntroduction">myAdmin</Link>
+          <div className={`tab-item ${isActive("myAdmin")}`}>
+            <Link
+              to="/myAdminIntroduction"
+              onClick={() => handleLinkClick("myAdmin")}
+            >
+              myAdmin
+            </Link>
           </div>
-          <div className="tab-item">
-            <Link to="/driveIntroduction">Drive</Link>
+          <div className={`tab-item ${isActive("Drive")}`}>
+            <Link
+              to="/driveIntroduction"
+              onClick={() => handleLinkClick("Drive")}
+            >
+              Drive
+            </Link>
           </div>
-          <div className="tab-item">
-            <Link to="/hardwareIntroduction">Hardware</Link>
+          <div className={`tab-item ${isActive("Hardware")}`}>
+            <Link
+              to="/hardwareIntroduction"
+              onClick={() => handleLinkClick("Hardware")}
+            >
+              Hardware
+            </Link>
           </div>
         </div>
 
-        <SearchBar className="searchbar" />
+        <div className="search-bar-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            onClick={openSearchModal}
+            className="search-bar-input"
+          />
+        </div>
+        <SearchModal isOpen={isSearchModalOpen} onClose={closeSearchModal} />
       </header>
     </MenuContext.Provider>
   );
---
+++ src/components/Header/SearchModal.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface SearchModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function SearchModal({ isOpen, onClose }: SearchModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <p>Search Modal opened</p>
+        <button onClick={onClose}>X</button>
+      </div>
+    </div>
+  );
+}
