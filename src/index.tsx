3f52d383f40d7c87ed448a45ab26c6f3a0970cbd
williamwgeotab <yaopanwong@geotab.com>
2023-10-24 18:03:02 -0400

    INTGR-141: style/fixed font issue - import App.scss to index.tsx to fix font issue - minor refactoring for header styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MenuProvider } from "./menuContext";
 import "./index.css";
+import './App.scss';
 import App from "./App";
 
 const root = ReactDOM.createRoot(
