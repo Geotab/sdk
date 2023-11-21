051b30b86d45378ec768d25f753983942a4ba070
williamwgeotab <146376294+williamwgeotab@users.noreply.github.com>
2023-11-20 21:54:15 -0500

    INTGR: feat/Implement Footer component (#356)
    * INTGR: feat/Implement Footer component
    - Add initial footer component content
    - Add styling matching figma
    - Add logo and divider svg
    - Add footer component to App.tsx
    
    * style: Implement sticky footer in App component
    
    * refactor: enhance accessibility and link behavior in Footer component
    
    * refactor: improve footer component to be more reusable
    - made component more typescript friendly
    - fix styling for logo to be more aligned

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { FooterDivider, LogoGeotabFooter } from "../Footer";
+import "./footer.scss";
+
+interface FooterLink {
+  href: string;
+  label: string;
+}
+
+const footerLinks: FooterLink[] = [
+  { href: "https://www.geotab.com/security", label: "Security" },
+  {
+    href: "https://docs.google.com/document/d/1sVygLN02w2xNovFY4q5vw-oAzfYxCd7WLhyToElgDbs/pub",
+    label: "Privacy Policy",
+  },
+  {
+    href: "https://docs.google.com/document/d/1jA8Qc8WZMhmaqdjd8Ng8rY4iMI073xCfLNt_DSP72TQ/edit#heading=h.gjdgxs",
+    label: "End User Agreement",
+  },
+  {
+    href: "https://geotab.github.io/sdk/software/guides/addin-storage/",
+    label: "Storage Preferences",
+  },
+];
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <div className="footer-container">
+          <div className="footer-container__left">
+            <LogoGeotabFooter />
+            {footerLinks.map((link, index) => (
+              <div key={link.label} className="link-container">
+                <a
+                  href={link.href}
+                  aria-label={link.label}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {link.label}
+                </a>
+                <span className="footer-link-divider">
+                  {index < footerLinks.length - 1 && <FooterDivider />}
+                </span>
+              </div>
+            ))}
+          </div>
+
+          <div className="footer-container__right">
+            <small className="footer-copyright">Copyright © 2023 Geotab</small>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 export {default as Header} from "./Header/Header";
 export {default as Navbar} from "./Navbar/Navbar";
+export {default as Footer} from "./Footer/Footer";
