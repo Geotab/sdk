7a30adc5d1b5fd9e1a4c4f622dc59f7cf36b25be
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2023-11-21 12:34:36 -0800

    feat: Add scroll highlight to ToC  - When scrolling the page, highlight the item in the table that corresponds to    what part of the page you're on

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./tableOfContents.scss";
 
 export interface TableOfContentsItem {
@@ -12,6 +12,35 @@ interface TableOfContentsProps {
 }
 
 export default function TableOfContents(props: TableOfContentsProps) {
+    const [activeSection, setActiveSection] = useState("");
+    const observer = useRef() as React.MutableRefObject<IntersectionObserver>;
+
+    // useEffect hook implementation copied from:
+    // https://netacci.hashnode.dev/how-to-highlight-active-navigation-on-scroll-in-react#heading-intersection-observer-api
+    useEffect(() => {
+        //create new instance and pass a callback function
+        observer.current = new IntersectionObserver((entries) => {
+            const visibleSection = entries.find((entry) => entry.isIntersecting)?.target;
+            //Update state with the visible section ID
+            if (visibleSection) {
+                setActiveSection(visibleSection.id);
+            }
+        });
+
+        //Get custom attribute data-section from all sections
+        const sections = document.querySelectorAll('details div.paragraph');
+
+        sections.forEach((section) => {
+            observer.current.observe(section);
+        });
+        //Cleanup function to remove observer
+        return () => {
+            sections.forEach((section) => {
+                observer.current.unobserve(section);
+            });
+        };
+    }, []);
+
     return (
         <ul className="tableOfContents">
             <li className="tableOfContents__heading">
@@ -19,7 +48,7 @@ export default function TableOfContents(props: TableOfContentsProps) {
             </li>
             {props.items.map((item: TableOfContentsItem) => {
                 return (
-                    <li key={item.elementId}>
+                    <li key={item.elementId} className={activeSection === item.elementId ? "tableOfContents__item--active": ""}>
                         <a href={`#${item.elementId}`}>{item.summary}</a>
                     </li>
                 );
