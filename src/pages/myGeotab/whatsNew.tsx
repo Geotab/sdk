d114296801d226fe33dca8502c0b9444dd54d8eb
Wajih Qazi <wajihqazi@geotab.com>
2023-11-22 17:57:12 -0500

    feat/ Initial commit of code for release notes page

--- src/pages/myGeotab/whatsNew.tsx
+++ src/pages/myGeotab/whatsNew.tsx
@@ -1,8 +1,28 @@
- 
+import { ReactNode } from 'react';
+import Accordian from '../../components/Accordion/Accordion';
+import "../../pages/pages.scss";
+import InformationalBox from '../../components/InformationalBox/InformationalBox';
+import CodeSample from  "../../components/CodeSamplesContainer/CodeSample";
+import { IconChevronRightSmall } from "@geotab/react-component-library";
+
 export default function WhatsNew () {
     return (
-      <div>
-        <h1>Everything's broken now.</h1>
-      </div>
+        <div className="page-container">
+            <div className="grayBackground">
+				<div className="breadCrumb">
+					<span>MYG</span>
+					<IconChevronRightSmall></IconChevronRightSmall>
+					<span>Release Notes</span>
+					<IconChevronRightSmall></IconChevronRightSmall>
+				</div>
+				<h1>Release Notes</h1>
+				{/* <Button variant="primary" onClick={function subscribe() {}})></Button> */}
+            </div>
+			<div className="paragraph">
+				<p>
+
+				</p>
+			</div>
+        </div>
     );
-  };
+};
