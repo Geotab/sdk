8eda9ad8dc3674c97e472190da653b48a1f46bb4
kaylanhenley <kaylanhenley@KMETT01-PER-02>
2023-11-29 11:13:34 -0800

    fix: fine you tyrant

--- src/pages/myGeotab/guides/codeBase/usingInDotnet.tsx
+++ src/pages/myGeotab/guides/codeBase/usingInDotnet.tsx
@@ -8,15 +8,15 @@ import InformationalBox from "../../../../components/InformationalBox/Informatio
 import CodeSample from "../../../../components/CodeSamplesContainer/CodeSample";
 
 const nugetPackage: ReactNode = <div className="paragraph">
-    <p>The <a href="https://www.nuget.org/packages/Geotab.Checkmate.ObjectModel/" target="_blank" rel="noopener noreferrer">nuget package</a> is an SDK library for accessing MyGeotab customer databases. 
-    It is a convenient "wrapper" around Geotab's HTTP/JSON API to allow developers focus on writing code instead of moving data over the wire. It includes tools to assist authenticating against Geotab's 
-    servers, automatically serializing/deserializing JSON, and providing definitions for Checkmate object classes.</p>
+    <p>The <a href="https://www.nuget.org/packages/Geotab.Checkmate.ObjectModel/" target="_blank" rel="noopener noreferrer">nuget package</a> is an SDK library for accessing MyGeotab customer databases.
+        It is a convenient "wrapper" around Geotab's HTTP/JSON API to allow developers focus on writing code instead of moving data over the wire. It includes tools to assist authenticating against Geotab's
+        servers, automatically serializing/deserializing JSON, and providing definitions for Checkmate object classes.</p>
     <p>Please remember to regularly check for Geotab.Checkmate.ObjectModel nuget package updates. Ideally, your integration should use the same Geotab.Checkmate.ObjectModel nuget package version
-    as the one that your database is on.</p>
+        as the one that your database is on.</p>
     <InformationalBox>
         <p>Quick start in <a href="../../api/clients">{/*TODO: fix link*/}API Clients</a></p>
     </InformationalBox>
-</div>
+</div>;
 
 const apiClass: ReactNode = <div className="paragraph">
     <h2>Step 1: Initialize & Authentication</h2>
@@ -27,14 +27,14 @@ const apiClass: ReactNode = <div className="paragraph">
             `using Geotab.Checkmate;
 using Geotab.Checkmate.ObjectModel;`
         }
-    ></CodeSample>
+    />
     <p>An instance of API can now be constructed to be used in the code. For the most basic use case, all the data that is needed is user credentials and a database name:</p>
     <CodeSample
         language="csharp"
         code={
             `var api = new API(userName, password, null, databaseName, server);`
         }
-    ></CodeSample>
+    />
     <p>At this point there has not been any communication with Geotab's servers. In order to make calls to Geotab's API, an authentication call must be made:</p>
     <CodeSample
         language="csharp"
@@ -62,10 +62,10 @@ using Geotab.Checkmate.ObjectModel;`
     }
 });`
         }
-    ></CodeSample>
-    <p>The last parameter to this Call method is an <a href="http://msdn.microsoft.com/en-us/library/bb397696.aspx" target="_blank" rel="noopener noreferrer">anonymous object</a> which contains the 
-    parameters for the method (please review the <a href="../../api/reference/#Remove1">API reference</a> to see which parameters the method expects, and whether the parameters are required or optional). 
-    The parameter order is not significant, and it is acceptable to omit optional parameters. Optional parameters will revert to their default values, typically "null" or “false” values.</p>
+    />
+    <p>The last parameter to this Call method is an <a href="http://msdn.microsoft.com/en-us/library/bb397696.aspx" target="_blank" rel="noopener noreferrer">anonymous object</a> which contains the
+        parameters for the method (please review the <a href="../../api/reference/#Remove1">API reference</a> to see which parameters the method expects, and whether the parameters are required or optional).
+        The parameter order is not significant, and it is acceptable to omit optional parameters. Optional parameters will revert to their default values, typically "null" or “false” values.</p>
     <p>The API class automatically handles databases that are moved to different servers in the federation and expired tokens (token are typically valid for 2 weeks) by automatically
         re-authenticating and continuing.</p>
     <h2>Example Code</h2>
@@ -103,7 +103,7 @@ public static async Task Main(string[] args) {
     }
 }`
         }
-    ></CodeSample>
+    />
     <InformationalBox>
         <p>Hint: async main method requires <a href="https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-7-1" target="_blank" rel="noopener noreferrer">C# 7.1</a>.</p>
     </InformationalBox>
@@ -117,13 +117,13 @@ G70000000001    43.4371071      -79.7124329
 G70000000002    43.3683701      -79.7784042
 G80000000003    43.4620934      -79.6879883`
         }
-    ></CodeSample>
-</div>
+    />
+</div>;
 
 const nextSteps: ReactNode = <div className="paragraph">
     Once you have a basic understanding of how the .NET SDK works, we recommend reviewing the examples that we have created
     <a href="https://github.com/Geotab/sdk-dotnet-samples" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+</div>;
 
 const pageTitle: PageTitleProps = {
     "title": "Using In .NET",
@@ -152,8 +152,8 @@ export default function usingInDotnet() {
     return (
         <Page section={HeaderSections.MyGeotab} pageTitle={pageTitle} tableOfContents={pageSections}>
             <div className="paragraph">
-                The .NET SDK tools provide an easy way to integrate MyGeotab into .NET software. All of the communication to Geotab's services is accomplished over HTTPS with data serialized in 
-                {" "}<a href="http://en.wikipedia.org/wiki/JSON" target="_blank" rel="noopener noreferrer">JSON</a> format. The .NET library provided will automatically handle the JSON serialization 
+                The .NET SDK tools provide an easy way to integrate MyGeotab into .NET software. All of the communication to Geotab's services is accomplished over HTTPS with data serialized in
+                {" "}<a href="http://en.wikipedia.org/wiki/JSON" target="_blank" rel="noopener noreferrer">JSON</a> format. The .NET library provided will automatically handle the JSON serialization
                 and deserialization into MyGeotab objects.
             </div>
 
