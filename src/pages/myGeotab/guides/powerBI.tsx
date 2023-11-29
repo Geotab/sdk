ccf32764d78ef4209fca085ff8eaf9c3a717d286
williamwgeotab <yaopanwong@geotab.com>
2023-11-28 20:56:47 -0500

    INTGR-223: refactor/Data Feed & Power BI page - Add new page component - Add pageTitle & pageSections - Improve external href links

--- src/pages/myGeotab/guides/powerBI.tsx
+++ src/pages/myGeotab/guides/powerBI.tsx
@@ -1,39 +1,59 @@
 import { ReactNode } from "react";
 import Accordion from "../../../components/Accordion/Accordion";
-import { IconChevronRightSmall } from "@geotab/react-component-library";
+import { Page } from "../../../components";
+import { PageTitleProps } from "../../../components/PageTitle/PageTitle";
+import { HeaderSections } from "../../../components/Header/headerSectionsEnum";
+import { TableOfContentsItem } from "../../../components/TableOfContents/TableOfContents";
 import "../../../pages/pages.scss";
 
 const gettingStarted: ReactNode = (
   <div className="paragraph">
     <p>
       To connect Power BI to the MyGeotab SDK, please refer to{" "}
-      <a href="https://docs.google.com/document/d/1yU-OMC5-DJoRaisB3_JOm0YS99Y26C34NxvJZ7ZM3J8/edit">
+      <a
+        href="https://docs.google.com/document/d/1yU-OMC5-DJoRaisB3_JOm0YS99Y26C34NxvJZ7ZM3J8/edit"
+        aria-label="Power BI Geotab Connection Guide"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Power BI Geotab Connection with Adapter
       </a>
     </p>
     <p>
       To connect Power BI to the MyGeotab SDK using the Power Query M formula
       language, please refer to{" "}
-      <a href="https://docs.google.com/document/d/1ikEi3HCaSugzaCkT78e1Lk5ySMctzfMak_rQazC6DUk">
+      <a
+        href="https://docs.google.com/document/d/1ikEi3HCaSugzaCkT78e1Lk5ySMctzfMak_rQazC6DUk"
+        aria-label="Geotab API Integration Guide for Power BI using Power Query M"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Geotab API integration for Power BI
       </a>
     </p>
   </div>
 );
 
+const pageTitle: PageTitleProps = {
+  title: "Connect To Power BI",
+  breadCrumbItems: ["MYG", "Guides", "Power BI"],
+};
+
+const pageSections: TableOfContentsItem[] = [
+  {
+    elementId: "getting-started",
+    summary: "Getting Started",
+    details: gettingStarted,
+  },
+];
+
 export default function DataFeed() {
   return (
-    <div className="pageContent">
-      <div className="grayBackground">
-        <div className="breadCrumb">
-          <span>MYG</span>
-          <IconChevronRightSmall></IconChevronRightSmall>
-          <span>Guides</span>
-          <IconChevronRightSmall></IconChevronRightSmall>
-          <span>Power BI</span>
-        </div>
-        <h1 className="title">Connect To Power BI</h1>
-      </div>
+    <Page
+      section={HeaderSections.MyGeotab}
+      pageTitle={pageTitle}
+      tableOfContents={pageSections}
+    >
       <div className="paragraph">
         <p>
           Microsoft Power BI provides data visualization and analytics tools
@@ -43,7 +63,13 @@ export default function DataFeed() {
         </p>
       </div>
 
-      <Accordion summary="Getting Started" p={gettingStarted} />
-    </div>
+      {pageSections.map((section) => (
+        <Accordion
+          summary={section.summary}
+          p={section.details}
+          id={section.elementId}
+        />
+      ))}
+    </Page>
   );
 }
