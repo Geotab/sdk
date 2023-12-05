b16a4d73c34e77a4af9cc9428c9cce1e442d2c2f
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2023-12-05 01:07:23 -0800

    INTGR-214: feat/Dark/light mode for code samples  - Introduce new context for dark/light mode  - Rename CustomTheme to CustomDarkModeTheme  - Add CustomLightModeTheme  - Add necessary icons for each mode  - Update copy icon to work for both modes

--- src/components/CodeSamplesContainer/CodeSample.tsx
+++ src/components/CodeSamplesContainer/CodeSample.tsx
@@ -1,50 +1,81 @@
 import copyCode from "./CopyCode";
-import { CodeBlock } from 'react-code-blocks';
-import './CodeSample.scss';
-import copyIcon from "./CopyIcon";
-import CustomTheme from "./CustomTheme";
+import { CodeBlock } from "react-code-blocks";
+import "./CodeSample.scss";
+import CopyIcon from "./CopyIcon";
+import CustomDarkModeTheme from "./CustomDarkModeTheme";
 import CodeSampleStyle from "./CodeSampleStyle";
-import { Button, ButtonVariant,useToast } from "@geotab/react-component-library";
+import {
+  Button,
+  ButtonVariant,
+  useToast,
+} from "@geotab/react-component-library";
 import "../../App.scss";
+import LightModeIcon from "./LightModeIcon";
+import { useContext } from "react";
+import DarkModeIcon from "./DarkModeIcon";
+import CodeSampleLightDarkModeContext from "./CodeSampleLightDarkModeContext";
+import CustomLightModeTheme from "./CustomLightModeTheme";
 
 //Documentation for react-code-blocks: https://www.npmjs.com/package/react-code-blocks
 
 interface CodeSampleProps {
-    language: string;
-    code: string;
-  }
+  language: string;
+  code: string;
+}
 
-export default function CodeSample(props: CodeSampleProps): JSX.Element{
+export default function CodeSample(props: CodeSampleProps): JSX.Element {
+  const { lightMode, setLightMode } = useContext(
+    CodeSampleLightDarkModeContext
+  );
+  const toast = useToast();
 
-    const toast = useToast();
+  const handleCopyClick = (): void => {
+    copyCode(props.code);
+    toast.info({
+      sensitivity: "foreground",
+      body: "Copied to clipboard",
+      duration: 2000,
+      icon: null,
+    });
+  };
 
-    const handleCopyClick = (): void => {
-      copyCode(props.code);
-      toast.info({ 
-        sensitivity: "foreground", 
-        body: "Copied to clipboard", 
-        duration: 2000,
-        icon: null
-      });
-    };
+  const handleLightDarkModeToggleButtonClick = (): void => {
+    setLightMode(!lightMode);
+  };
 
-    return (
+  return (
     <div className="snippetBox">
       <CodeBlock
-          text={props.code}
-          language= {props.language}
-          showLineNumbers={false}
-          theme={	CustomTheme() }
-          customStyle={ CodeSampleStyle() }
+        text={props.code}
+        language={props.language}
+        showLineNumbers={false}
+        theme={lightMode ? CustomLightModeTheme() : CustomDarkModeTheme()}
+        customStyle={CodeSampleStyle()}
       />
 
-      <Button variant = {ButtonVariant.CheveronButton} className="copyButton" onClick={handleCopyClick} ariaLabel="Copy code sample button">
-          {copyIcon()}
-      </Button>
+      <div className="snippetBox__toolsContainer">
+        <Button
+          variant={ButtonVariant.CheveronButton}
+          className={
+            lightMode
+              ? "lightDarkModeToggleButton"
+              : "lightDarkModeToggleButton--dark-mode"
+          }
+          onClick={handleLightDarkModeToggleButtonClick}
+          ariaLabel="Toggle light/dark mode"
+        >
+          {lightMode ? DarkModeIcon() : LightModeIcon()}
+        </Button>
 
+        <Button
+          variant={ButtonVariant.CheveronButton}
+          className={lightMode ? "copyButton" : "copyButton--dark-mode"}
+          onClick={handleCopyClick}
+          ariaLabel="Copy code sample button"
+        >
+          {CopyIcon()}
+        </Button>
+      </div>
     </div>
-      
-    )
-  } 
-
-
+  );
+}
