1a9a75eb996af70f497fa589b5ef0b7b5619b104
Wajih Qazi <wajihqazi@geotab.com>
2023-12-07 14:07:00 -0500

    Initial commit of usingWithDotnet MyA Page

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import MyGeotabObjects from "./pages/myGeotab/apiReference/objects";
 import MyGeotabMethod from "./pages/myGeotab/apiReference/method";
 
 import MyAdminIntroduction from "./pages/myAdmin/introduction";
+import UsingWithDotnet from "./pages/myAdmin/guides/usingWithDotnet";
 import HardwareIntroduction from "./pages/hardware/introduction";
 
 import DriveIntroduction from "./pages/drive/introduction";
@@ -69,6 +70,7 @@ function App() {
           <Route path="/myGeotab/apiReference/objects" Component={MyGeotabObjects} />
 
           <Route path="/myAdmin/introduction" Component={MyAdminIntroduction} />
+          <Route path="/myAdmin/guides/usingWithDotnet" Component={UsingWithDotnet}/>
           <Route path="/hardware/introduction" Component={HardwareIntroduction} />
 
           <Route path="/drive/introduction" Component={DriveIntroduction} />
