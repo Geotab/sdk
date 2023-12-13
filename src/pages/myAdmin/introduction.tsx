3062f29be7743c71f62bd876b4fd935ceaf07456
williamwgeotab <yaopanwong@geotab.com>
2023-12-12 21:01:27 -0500

    INTGR-239: fix/ HeaderSections.MyGeotab -> MyAdmin and rename function to Introduction

--- src/pages/myAdmin/introduction.tsx
+++ src/pages/myAdmin/introduction.tsx
@@ -1,16 +1,16 @@
 import { ReactNode } from "react";
-import Accordion from "../../components/Accordion/Accordion";
 import { Page } from "../../components";
 import { PageTitleProps } from "../../components/PageTitle/PageTitle";
 import { HeaderSections } from "../../components/Header/headerSectionsEnum";
 import { TableOfContentsItem } from "../../components/TableOfContents/TableOfContents";
+import Accordion from "../../components/Accordion/Accordion";
 import "../../pages/pages.scss";
 
 const myAdminNextSteps: ReactNode = (
   <div className="paragraph">
-Access to the MyAdmin API requires a MyAdmin account with the MyAdminApiUser role. It is recommended that Resellers and Partners set up a new, dedicated account to access the API. If desired, the MyAdminApiUser role can be added to an existing MyAdmin account. A new account can be registered at <a href="https://myadmin.geotab.com" target="_blank" rel="noreferrer">myadmin.geotab.com</a>, after which Resellers or Partners can contact Geotab to add the MyAdminApiUser role to their account.
+    Access to the MyAdmin API requires a MyAdmin account with the MyAdminApiUser role. It is recommended that Resellers and Partners set up a new, dedicated account to access the API. If desired, the MyAdminApiUser role can be added to an existing MyAdmin account. A new account can be registered at <a href="https://myadmin.geotab.com" target="_blank" rel="noreferrer">myadmin.geotab.com</a>, after which Resellers or Partners can contact Geotab to add the MyAdminApiUser role to their account.
 
-Please review the <a href="../myAdmin/guides/gettingStarted">Getting Started</a> and <a href="../myAdmin/guides/concepts">Concepts</a> sections prior to beginning development. {/* TODO: fix link when page is available*/} <a href="../code-samples/javascript-examples/">JavaScript</a> and {/* TODO: fix link when page is available*/} <a href="../code-samples/dotnet-examples/">.NET</a> examples have been provided to help you get started.
+    Please review the <a href="../myAdmin/guides/gettingStarted">Getting Started</a> and <a href="../myAdmin/guides/concepts">Concepts</a> sections prior to beginning development. {/* TODO: fix link when page is available*/} <a href="../code-samples/javascript-examples/">JavaScript</a> and {/* TODO: fix link when page is available*/} <a href="../code-samples/dotnet-examples/">.NET</a> examples have been provided to help you get started.
   </div>
 );
 
@@ -27,9 +27,9 @@ const pageSections: TableOfContentsItem[] = [
   }
 ];
 
-export default function MyAdminIntroduction() {
+export default function Introduction() {
   return (
-    <Page section={HeaderSections.MyGeotab} pageTitle={pageTitle} tableOfContents={pageSections}>
+    <Page section={HeaderSections.MyAdmin} pageTitle={pageTitle} tableOfContents={pageSections}>
       <div className="paragraph">
         The MyAdmin API provides an interface to Geotab's MyAdmin system allowing Geotab Resellers and Partners to build their own applications to integrate with MyAdmin. Some tasks that can be accessed through the API include:
         <ul>
