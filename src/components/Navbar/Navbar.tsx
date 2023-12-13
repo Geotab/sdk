5bad1f3d601c40661e6709b80adb496f6c612039
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2023-12-13 13:11:22 -0800

    INTGR-142: feat/Initial Navbar implementation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,94 +2,92 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { MenuContext } from "../../menuContext";
 import {
-  SideNavigationBase,
+  SideNavigation,
   SideNavigationCollapseProvider,
+  SideNavigationMenuItem
 } from "@geotab/react-component-library";
 import { LogoGeotabSDK } from "../Logo/LogoGeotabSDK";
+import * as Tooltip from "@radix-ui/react-tooltip";
+import { useNavigate } from "react-router";
 
-export default function Navbar(props: anyprops: any) {
-  const section = useContext(MenuContext);
+export default function Navbar(props: any) {
+  const navigate = useNavigate();
+  const handleLogoClick = () => navigate('/sdk');
 
   return (
     <div>
-      <SideNavigationCollapseProvider>
-        <SideNavigationBase>
-          <SideNavigationBase.Main Logo={LogoGeotabSDK} />
+      <Tooltip.Provider delayDuration={350}>
+        <SideNavigationCollapseProvider>
+          <SideNavigation>
+            <SideNavigation.Main Logo={LogoGeotabSDK} onLogoClick={handleLogoClick} />
+          </SideNavigation>
+        </SideNavigationCollapseProvider>
+      </Tooltip.Provider>
+      {/* <div>
+        <Link to="/myGeotab/introduction">Introduction</Link>
+      </div>
+      <div>
+        <Link to="/myGeotab/whatsNew">What's New</Link>
+      </div>
+      <div>
+        Guides
+        <div>
+          <Link to="/myGeotab/guides/gettingStarted">Getting Started</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/guides/dataFeed">Data Feed</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/guides/goDeviceLogging">GO Device Logging</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/guides/usingGoDevices">Using GO Devices</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/guides/usingCustomTelematicsDevices">Using Custom Telematics Devices</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/guides/powerBI">Connect to Power BI</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/guides/myGeotabUrls">Using MyGeotab URLs</Link>
+        </div>
+        <div>Code Base
           <div>
-            <Link to="/myGeotab/introduction">Introduction</Link>
+            <Link to="/myGeotab/guides/codeBase/usingInDotnet">Using In .NET</Link>
           </div>
           <div>
-            <Link to="/myGeotab/whatsNew">What's New</Link>
+            <Link to="/myGeotab/guides/codeBase/usingInJavascript">Using In JavaScript</Link>
           </div>
-          <div>
-            Guides
-            <div>
-              <Link to="/myGeotab/guides/gettingStarted">Getting Started</Link>
-            </div>
-            <div>
-              <Link to="/myGeotab/guides/dataFeed">Data Feed</Link>
-            </div>
-            <div>
-              <Link to="/myGeotab/guides/goDeviceLogging">GO Device Logging</Link>
-            </div>
-            <div>
-              <Link to="/myGeotab/guides/usingGoDevices">Using GO Devices</Link>
-            </div>
-            <div>
-              <Link to="/myGeotab/guides/usingCustomTelematicsDevices">Using Custom Telematics Devices</Link>
-            </div>
-            <div>
-              <Link to="/myGeotab/guides/powerBI">Connect to Power BI</Link>
-            </div>
-            <div>
-              <Link to="/myGeotab/guides/myGeotabUrls">Using MyGeotab URLs</Link>
-            </div>
-            <div>
-              <Link to="/myGeotab/guides/mgMediaFiles">Media File - Beta</Link>
-              <div>Code Base
-                <div>
-                  <Link to="/myGeotab/guides/codeBase/usingInDotnet">Using In .NET</Link>
-                </div>
-                <div>
-                  <Link to="/myGeotab/guides/codeBase/usingInJava">Using In Java</Link>
-                </div>
-                <div>
-                  <Link to="/myGeotab/guides/codeBase/usingInJavascript">Using In JavaScript</Link>
-                </div>
-              </div>
-            </div >
-            <div>
-              Add-Ins
-              <div>
-                <Link to="/myGeotab/addIns/developingAddIns">Developing Add-Ins</Link>
-              </div>
-              <div>
-                <Link to="/myGeotab/addIns/addInStorage">Using Add-Ins for Data Storage</Link>
-              </div>
-              <div>
-                <Link to="/myGeotab/addIns/mapAddIns">Map Add-Ins</Link>
-              </div>
-            </div>
-            <div>
-              API Reference
-              <div>
-                <Link to="/myGeotab/apiReference/methods">Methods</Link>
-              </div>
-              <div>
-                <Link to="/myGeotab/apiReference/objects">Objects</Link>
-              </div>
-            </div>
-            <div>
-              <Link to="/myGeotab/apiClients">API Clients</Link>
-            </div>
-            <div>
-              Drive
-              <div>
-                <Link to="/drive/addIns/driveAddIns">Drive Add-Ins</Link>
-              </div>
-            </div>
-        </SideNavigationBase>
-      </SideNavigationCollapseProvider>
+        </div>
+      </div >
+      <div>
+        Add-Ins
+        <div>
+          <Link to="/myGeotab/addIns/developingAddIns">Developing Add-Ins</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/addIns/addInStorage">Using Add-Ins for Data Storage</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/addIns/mapAddIns">Map Add-Ins</Link>
+        </div>
+      </div>
+      <div>
+        API Reference
+        <div>
+          <Link to="/myGeotab/apiReference/methods">Methods</Link>
+        </div>
+        <div>
+          <Link to="/myGeotab/apiReference/objects">Objects</Link>
+        </div>
+      </div>
+      <div>
+        Drive
+        <div>
+          <Link to="/drive/addIns/driveAddIns">Drive Add-Ins</Link>
+        </div>
+      </div> */}
     </div >
   );
 }
