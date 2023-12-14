2a03314d7c4a44f1c988a53e67b82c0beaa7648c
Wajih Qazi <wajihqazi@geotab.com>
2023-12-14 01:07:40 -0500

    fix/Remove unnecessary import

--- src/pages/hardware/developingAnIOX/MIMEProtocol.tsx
+++ src/pages/hardware/developingAnIOX/MIMEProtocol.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
 import Accordion from "../../../components/Accordion/Accordion";
-import InformationalBox from "../../../components/InformationalBox/InformationalBox";
 import CodeSample from "../../../components/CodeSamplesContainer/CodeSample";
 import { Page } from "../../../components";
 import { PageTitleProps } from "../../../components/PageTitle/PageTitle";
