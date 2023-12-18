96c1d446ecd95d60436346cea33b1ba96d9cf1b2
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2023-12-18 15:13:02 -0800

    INTGR-142: feat/Initial Drive navbar implementation

--- src/components/Navbar/DriveNavbarItems.ts
+++ src/components/Navbar/DriveNavbarItems.ts
@@ -24,22 +24,31 @@ export const DriveNavbarItems: SideNavigationMenuItemType[] = [
     {
         id: 0,
         label: "Introduction",
-        route: "/myGeotab/introduction",
+        route: "/drive/introduction",
         level: 0,
-        order: 1,
-        userTypes: ["admin", "user"],
-        roles: ["admin", "user"],
-        Icon: IconCircleInfo,
-        onClick: () => alert("Introduction clicked")
+        order: 0,
+        userTypes: [],
+        roles: [],
+        Icon: IconCircleInfo
     },
     {
         id: 1,
         label: "Add-Ins",
         level: 0,
-        order: 2,
-        userTypes: ["admin", "user"],
-        roles: ["admin", "user"],
+        order: 1,
+        userTypes: [],
+        roles: [],
         Icon: IconPuzzlePiece,
-        children: []
+        children: [
+            {
+                id: 0,
+                label: "Add-Ins for Drive",
+                route: "/drive/addIns/driveAddIns",
+                level: 1,
+                order: 0,
+                userTypes: [],
+                roles: []
+            }
+        ]
     }
 ];
