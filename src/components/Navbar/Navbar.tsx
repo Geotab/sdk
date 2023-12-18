8b2d72bd387a639dc7bc041e0d60dfaa12003a0d
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2023-12-18 15:18:56 -0800

    INTGR-142: refactor/Clean up Navbar file

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -66,7 +66,6 @@ export default function Navbar(props: any) {
         <SideNavigationCollapseProvider>
           <SideNavigation>
             <SideNavigation.Main Logo={LogoGeotabSDK} onLogoClick={handleLogoClick} />
-            {/* <SideNavigation.Primary menuItems={MyGeotabNavbarItems} /> */}
             <SideNavigation.Primary menuItems={navBarMapping[active]} />
             {/* It's necessary to pass in an empty array for the Secondary section so that the nav bar displays properly */}
             <SideNavigation.Secondary menuItems={[]} />
@@ -74,75 +73,6 @@ export default function Navbar(props: any) {
           </SideNavigation>
         </SideNavigationCollapseProvider>
       </Tooltip.Provider>
-      {/* <div>
-        <Link to="/myGeotab/introduction">Introduction</Link>
-      </div>
-      <div>
-        <Link to="/myGeotab/whatsNew">What's New</Link>
-      </div>
-      <div>
-        Guides
-        <div>
-          <Link to="/myGeotab/guides/gettingStarted">Getting Started</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/guides/concepts">Concepts</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/guides/usingCustomTelematicsDevices">Using Custom Telematics Devices</Link>
-          <Link to="/myGeotab/guides/dataFeed">Data Feed</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/guides/goDeviceLogging">GO Device Logging</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/guides/usingGoDevices">Using GO Devices</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/guides/usingCustomTelematicsDevices">Using Custom Telematics Devices</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/guides/powerBI">Connect to Power BI</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/guides/myGeotabUrls">Using MyGeotab URLs</Link>
-        </div>
-        <div>Code Base
-          <div>
-            <Link to="/myGeotab/guides/codeBase/usingInDotnet">Using In .NET</Link>
-          </div>
-          <div>
-            <Link to="/myGeotab/guides/codeBase/usingInJavascript">Using In JavaScript</Link>
-          </div>
-        </div>
-      </div >
-      <div>
-        Add-Ins
-        <div>
-          <Link to="/myGeotab/addIns/developingAddIns">Developing Add-Ins</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/addIns/addInStorage">Using Add-Ins for Data Storage</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/addIns/mapAddIns">Map Add-Ins</Link>
-        </div>
-      </div>
-      <div>
-        API Reference
-        <div>
-          <Link to="/myGeotab/apiReference/methods">Methods</Link>
-        </div>
-        <div>
-          <Link to="/myGeotab/apiReference/objects">Objects</Link>
-        </div>
-      </div>
-      <div>
-        Drive
-        <div>
-          <Link to="/drive/addIns/driveAddIns">Drive Add-Ins</Link>
-        </div>
-      </div> */}
     </div >
   );
 }
