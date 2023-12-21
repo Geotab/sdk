95eb4a6a937359b9581668828bdb77b36bbb922d
PCGeotab <philipcheung@geotab.com>
2023-12-21 14:12:56 -0800

    INTGR-266: Refactor code, split each h3 header to its own accordion section

--- src/pages/myGeotab/codeSamples/dotnetSamples.tsx
+++ src/pages/myGeotab/codeSamples/dotnetSamples.tsx
@@ -40,6 +40,7 @@ const howToRunTheExamples: ReactNode = (
           </li>
         </ul>
       </li>
+      <br />
       <li>
         Compile and run using the following commands
         <ul>
@@ -60,88 +61,116 @@ const howToRunTheExamples: ReactNode = (
   </div>
 );
 
-const examplesList: ReactNode = (
+const getCount: ReactNode = (
   <div className="paragraph">
-    <h3>Get count</h3>
     <p>
       A simple console example to obtain the count of devices from a database. A
       good example to see how our authentication scheme works.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/GetCount"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for get count folder"
+      >
+        Source
+      </a>
     </p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/GetCount"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for get count folder"
-    >
-      Source
-    </a>
-    <h3>Extract mileage</h3>
+  </div>
+);
+
+const extractMileage: ReactNode = (
+  <div className="paragraph">
     <p>
       An example that “extracts” vehicle mileage into a CSV or XML file. A good
       starting point for any data extraction tool.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ExtractMileage"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for extract mileage folder"
+      >
+        Source
+      </a>
     </p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ExtractMileage"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for extract mileage folder"
-    >
-      Source
-    </a>
-    <h3>Get logs</h3>
+  </div>
+);
+const getLogs: ReactNode = (
+  <div className="paragraph">
     <p>
       An example that obtains the logs for a given vehicle between a range of
       dates.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/GetLogs"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for get logs folder"
+      >
+        Source
+      </a>
     </p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/GetLogs"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for get logs folder"
-    >
-      Source
-    </a>
-    <h3>Get fuel tax details (IFTA)</h3>
-    <p>An example that shows how to:</p>
-    <ul>
-      <li>Iterate through a list of devices.</li>
-      <li>
-        Retrieve fuel tax details for each device over a given time interval.
-      </li>
-      <li>Trim each detail to the time interval.</li>
-    </ul>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/GetFuelTaxDetails"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for get fuel tax details folder"
-    >
-      Source
-    </a>
-    <h3>Text message</h3>
-    <p>An example that sends text messages to and from a GO device.</p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/TextMessage"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for text message folder"
-    >
-      Source
-    </a>
-    <h3>Import groups</h3>
+  </div>
+);
+
+const getFuelTaxDetailsIFTA: ReactNode = (
+  <div className="paragraph">
+    <p>
+      An example that shows how to:
+      <ul>
+        <li>Iterate through a list of devices.</li>
+        <li>
+          Retrieve fuel tax details for each device over a given time interval.
+        </li>
+        <li>Trim each detail to the time interval.</li>
+      </ul>
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/GetFuelTaxDetails"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for get fuel tax details folder"
+      >
+        Source
+      </a>
+    </p>
+  </div>
+);
+const textMessage: ReactNode = (
+  <div className="paragraph">
+    <p>
+      An example that sends text messages to and from a GO device.
+      <br/>
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/TextMessage"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for text message folder"
+      >
+        Source
+      </a>
+    </p>
+  </div>
+);
+const importGroups: ReactNode = (
+  <div className="paragraph">
     <p>
       A console example that is also a group import tool. It enables a one time
       import of groups to a database from a CSV file.
+      <br/>
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportGroups"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for import groups folder"
+      >
+        Source
+      </a>
     </p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportGroups"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for import groups folder"
-    >
-      Source
-    </a>
-    <h3>Reconcile groups - Alpha version</h3>
+  </div>
+);
+const reconcileGroupsAlphaVersion: ReactNode = (
+  <div className="paragraph">
     <p>
       A console example that is also a group import tool. Similar to the
       ImportGroups tool, it enables the initial import of groups into a database
@@ -170,31 +199,44 @@ const examplesList: ReactNode = (
     >
       Source
     </a>
-    <h3>Import devices</h3>
-    <p>Another console example that imports devices from a CSV file.</p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportDevices"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for import devices folder"
-    >
-      Source
-    </a>
-    <h3>Import zones</h3>
+  </div>
+);
+const importDevices: ReactNode = (
+  <div className="paragraph">
+    <p>
+      Another console example that imports devices from a CSV file.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportDevices"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for import devices folder"
+      >
+        Source
+      </a>
+    </p>
+  </div>
+);
+const importZones: ReactNode = (
+  <div className="paragraph">
     <p>
       A console example that imports zones from a CSV file. This is useful if
       you have a list of geographic coordinates and want to quickly create zones
       around them.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportZones"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for import zones folder"
+      >
+        Source
+      </a>
     </p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportZones"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for import zones folder"
-    >
-      Source
-    </a>
-    <h3>Import zones from shape file</h3>
+  </div>
+);
+const importZonesFromShapeFile: ReactNode = (
+  <div className="paragraph">
     <p>
       Another console zone importer, but imports from an{" "}
       <a
@@ -205,38 +247,49 @@ const examplesList: ReactNode = (
         Esri shapefile
       </a>{" "}
       set (.shp, .shx, .dbf) into a given database.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportZonesShapeFile"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for import zones shapefile folder"
+      >
+        Source
+      </a>
     </p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportZonesShapeFile"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for import zones shapefile folder"
-    >
-      Source
-    </a>
-    <h3>Import users</h3>
-    <p>Another console example that imports users from a CSV file.</p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportUsers"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for import users folder"
-    >
-      Source
-    </a>
-    <h3>Data feed</h3>
+  </div>
+);
+const importUsers: ReactNode = (
+  <div className="paragraph">
+    <p>
+      Another console example that imports users from a CSV file.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/ImportUsers"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for import users folder"
+      >
+        Source
+      </a>
+    </p>
+  </div>
+);
+const dataFeed: ReactNode = (
+  <div className="paragraph">
     <p>
       An example of retrieving GPS, Status and Fault data as a feed and
       exporting to a CSV file.
+      <br />
+      <a
+        href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/DataFeed"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Source for data feed folder"
+      >
+        Source
+      </a>
     </p>
-    <a
-      href="https://github.com/Geotab/sdk-dotnet-samples/tree/master/DataFeed"
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="Source for data feed folder"
-    >
-      Source
-    </a>
   </div>
 );
 
@@ -252,9 +305,64 @@ const pageSections: TableOfContentsItem[] = [
     details: howToRunTheExamples,
   },
   {
-    elementId: "examples-list",
-    summary: "Examples list",
-    details: examplesList,
+    elementId: "get-Count",
+    summary: "Get count",
+    details: getCount,
+  },
+  {
+    elementId: "extract-mileage",
+    summary: "Extract mileage",
+    details: extractMileage,
+  },
+  {
+    elementId: "get-logs",
+    summary: "Get logs",
+    details: getLogs,
+  },
+  {
+    elementId: "get-fuel-tax-details-IFTA",
+    summary: "Get fuel tax details (IFTA)",
+    details: getFuelTaxDetailsIFTA,
+  },
+  {
+    elementId: "text-message",
+    summary: "Text message",
+    details: textMessage,
+  },
+  {
+    elementId: "import-groups",
+    summary: "Import groups",
+    details: importGroups,
+  },
+  {
+    elementId: "reconcile-groups-alpha-version",
+    summary: "Reconcile groups - (Alpha version)",
+    details: reconcileGroupsAlphaVersion,
+  },
+  {
+    elementId: "import-devices",
+    summary: "Import devices",
+    details: importDevices,
+  },
+  {
+    elementId: "import-zones",
+    summary: "Import zones",
+    details: importZones,
+  },
+  {
+    elementId: "import-zones-from-shape-file",
+    summary: "Import zones from shape file",
+    details: importZonesFromShapeFile,
+  },
+  {
+    elementId: "import-users",
+    summary: "Import users",
+    details: importUsers,
+  },
+  {
+    elementId: "data-feed",
+    summary: "Data feed",
+    details: dataFeed,
   },
 ];
 
