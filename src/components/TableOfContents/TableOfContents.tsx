e6c3aa959dbdf7c15fc3d62d1f5268f269494e7a
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2023-12-28 16:20:47 -0800

    INTGR-269: feat/Initial implementation for ToC auto scroll

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./tableOfContents.scss";
+import { HashLink } from "react-router-hash-link";
 
 export interface TableOfContentsItem {
     summary: string;
@@ -18,13 +19,25 @@ export default function TableOfContents(props: TableOfContentsProps) {
     // useEffect hook implementation copied from:
     // https://netacci.hashnode.dev/how-to-highlight-active-navigation-on-scroll-in-react#heading-intersection-observer-api
     useEffect(() => {
-        //create new instance and pass a callback function
+        // Create new instance and pass a callback function
         observer.current = new IntersectionObserver((entries) => {
             const visibleSection = entries.find((entry) => entry.isIntersecting)?.target;
-            //Update state with the visible section ID
+            // Update state with the visible section ID & scroll to that section in the Table of Contents
             if (visibleSection) {
                 setActiveSection(visibleSection.id);
+
+                let tableOfContentsContainerViewport = document.querySelector('.tableOfContents')?.getBoundingClientRect();
+                let activeTableOfContentsItem = document.querySelector('li.tableOfContents__item--active');
+                let activeTableOfContentsItemViewport = activeTableOfContentsItem?.getBoundingClientRect();
+
+                // If the active table of contents item is not in the viewport, scroll to it
+                if (activeTableOfContentsItemViewport?.top! < tableOfContentsContainerViewport?.top! || activeTableOfContentsItemViewport?.bottom! > tableOfContentsContainerViewport?.bottom!) {
+                    activeTableOfContentsItem?.scrollIntoView({ block: "end" });
+                }
             }
+        }, {
+            // Set the threshold to 0.4 so that the active section changes when the section is 40% in the viewport
+            // threshold: 0.25,
         });
 
         const sections = document.querySelectorAll('details summary, details div.detailsContent');
@@ -47,8 +60,8 @@ export default function TableOfContents(props: TableOfContentsProps) {
             </li>
             {props.items.map((item: TableOfContentsItem) => {
                 return (
-                    <li key={item.elementId} className={activeSection === item.elementId ? "tableOfContents__item--active": ""}>
-                        <a href={`#${item.elementId}`}>{item.summary}</a>
+                    <li key={item.elementId} className={activeSection === item.elementId ? "tableOfContents__item--active" : ""}>
+                        <HashLink to={`#${item.elementId}`}>{item.summary}</HashLink>
                     </li>
                 );
             })}
