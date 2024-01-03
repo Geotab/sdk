8c085b2be13b5f4c41319e46d4471a45796bd3b3
Wajih Qazi <wajihqazi@geotab.com>
2024-01-03 07:58:10 -0500

    fix/Reverting import of Page.scss in Page.tsx

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import PageContent from "../PageContent/PageContent";
 import { PageTitleProps } from "../PageTitle/PageTitle";
-import "../../pages/pages.scss";
+import "./Page.scss";
 import { HeaderSections } from "../Header/headerSectionsEnum";
 import { TableOfContentsItem } from "../TableOfContents/TableOfContents";
 
