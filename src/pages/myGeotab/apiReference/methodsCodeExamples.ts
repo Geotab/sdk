6a4da0229e0f8f77825b1fa858f226cc07e0c1af
tranjennie <jennietran@geotab.com>
2024-01-10 13:52:57 -0800

    INTGR-289: feat / Add C# examples

--- src/pages/myGeotab/apiReference/methodsCodeExamples.ts
+++ src/pages/myGeotab/apiReference/methodsCodeExamples.ts
@@ -4,44 +4,44 @@ export const methodsCodeExamples: Record<string, CodeSamples> =
 {
     "GetCoordinates": {
         "javascript": "",
-        "csharp": "",
-        "python": "api.call('GetCoordinates', addresses= ['770 E Pilot Rd Suite A, Las Vegas, NV 89119', '2440 Winston Park Dr, Oakville, ON L6H 7V2, Canada'])",
+        "csharp": "await api.CallAsync<Coordinate[]>(\"GetCoordinates\", new { addresses = new[] { \"770 E Pilot Rd Suite A, Las Vegas, NV 89119\", \"21-1075 North Service Rd W, Oakville, ON L6M 2G2, Canada\" } });",
+        "python": "api.call(\"GetCoordinates\", addresses = [\"770 E Pilot Rd Suite A, Las Vegas, NV 89119\", \"2440 Winston Park Dr, Oakville, ON L6H 7V2, Canada\"])",
         "java": ""
     },
     "GetCountOf": {
         "javascript": "",
-        "csharp": "",
-        "python": "api.call('GetCountOf', typeName = 'Device')",
+        "csharp": "await api.CallAsync<int?>(\"GetCountOf\", typeof(Device))",
+        "python": "api.call(\"GetCountOf\", typeName = \"Device\")",
         "java": ""
     },
     "GetDaylightSavingRules": {
         "javascript": "",
-        "csharp": "",
-        "python": "api.call('GetDaylightSavingRules', timeZoneId = 'America/ Toronto')",
+        "csharp": "await api.CallAsync<TimeZoneInfoWithRules>(\"GetDaylightSavingRules\", new { timeZoneId = \"America/Toronto\" });",
+        "python": "api.call(\"GetDaylightSavingRules\", timeZoneId = \"America/ Toronto\")",
         "java": ""
     },
     "GetDirections": {
         "javascript": "",
-        "csharp": "",
-        "python": "api.call('GetDirections', waypoints = [{ 'coordinate': { 'x': -115.1477861, 'y': 36.0632094 }, 'sequence': 0, 'description': 'A' }, { 'coordinate': { 'x': -79.6863441, 'y': 43.5153199 }, 'sequence': 1, 'description': 'B' }])",
+        "csharp": "await api.CallAsync<Directions>(\"GetDirections\", new { waypoints = new List<Waypoint> { new Waypoint { Coordinate = new Coordinate { X = -115.1477861, Y = 36.0632094 }, Sequence = 0, Description = \"A\" }, new Waypoint { Coordinate = new Coordinate { X = -79.6863441, Y = 43.5153199 }, Sequence = 1, Description = \"B\" } } });",
+        "python": "api.call(\"GetDirections\", waypoints = [{ \"coordinate\": { \"x\": -115.1477861, \"y\": 36.0632094 }, \"sequence\": 0, \"description\": \"A\" }, { \"coordinate\": { \"x\": -79.6863441, \"y\": 43.5153199 }, \"sequence\": 1, \"description\": \"B\" }])",
         "java": ""
     },
     "GetFeed": {
         "javascript": "",
-        "csharp": "",
-        "python": "api.call('GetFeed', typeName = 'DutyStatusLog', fromVersion= '0')",
+        "csharp": "await api.CallAsync<FeedResult<DutyStatusLog>>(\"GetFeed\", typeof(DutyStatusLog), new { fromVersion = 0L });",
+        "python": "api.call(\"GetFeed\", typeName = \"DutyStatusLog\", fromVersion = \"0\")",
         "java": ""
     },
     "GetRoadMaxSpeeds": {
         "javascript": "",
-        "csharp": "",
-        "python": "api.call('GetRoadMaxSpeeds', deviceSearch = dict(id = 'b2'), fromDate= '2024-01-01T00:00:00.000Z', toDate= '2024-01-02T00:00:00.000Z')",
+        "csharp": "await api.CallAsync<Dictionary<DateTime, float>>(\"GetRoadMaxSpeeds\", new { deviceSearch = new DeviceSearch(Id.Create(\"b2\")), fromDate = new DateTime(2024, 01, 01, 00, 00, 00, DateTimeKind.Utc), toDate = new DateTime(2024, 01, 02, 00, 00, 00, DateTimeKind.Utc) });",
+        "python": "api.call(\"GetRoadMaxSpeeds\", deviceSearch = dict(id = \"b2\"), fromDate = \"2024-01-01T00:00:00.000Z\", toDate = \"2024-01-02T00:00:00.000Z\")",
         "java": ""
     },
     "GetSystemTimeUtc": {
         "javascript": "",
-        "csharp": "",
-        "python": "api.call('GetSystemTimeUtc')",
+        "csharp": "await api.CallAsync<DateTime>(\"GetSystemTimeUtc\")",
+        "python": "api.call(\"GetSystemTimeUtc\")",
         "java": ""
     },
 };
