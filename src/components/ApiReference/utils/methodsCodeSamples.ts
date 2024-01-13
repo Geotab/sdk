1e057b75f8fabfe28f8762d0898811fb453a91a2
Joseph Yalenkatian <josephyalenkatian@geotab.com>
2024-01-13 02:27:57 -0800

    INTGR-288: feat/methodsCodeSamples first set of code snippets

--- src/components/ApiReference/utils/methodsCodeSamples.ts
+++ src/components/ApiReference/utils/methodsCodeSamples.ts
@@ -3,27 +3,49 @@ import { CodeSamples } from "../../../components/CodeSamplesContainer";
 export const methodsCodeSamples: Record<string, CodeSamples> = {
     "Add": {
         "javascript": "",
-        "csharp": "",
+        "csharp": `IList<ZoneType>  zoneTypes = new List<ZoneType> { ZoneTypeOffice.Value };
+IList<ISimpleCoordinate> points = new List<ISimpleCoordinate> {
+    new Coordinate(x: -79.712318, y: 43.438266),
+    new Coordinate(x: -79.711181, y: 43.437461),
+    new Coordinate(x: -79.712677, y: 43.436168),
+    new Coordinate(x: -79.713966, y: 43.437107),
+    new Coordinate(x: -79.712318, y: 43.438266)
+};
+Geotab.Drawing.Color zoneColor = new Geotab.Drawing.Color(255, 165, 0, 191);
+IList<Group> groups = new List<Group> { new CompanyGroup() };
+
+Zone zone = new Zone(null, null, "Example zone " + DateTime.Now, null, true, zoneTypes, points, new DateTime(1986, 01, 01), new DateTime(2055, 01, 01), zoneColor, true, groups);
+
+await api.CallAsync<Id>(
+    "Add",
+    typeof(Zone),
+    new
+    {
+        entity = zone
+    }
+);`,
         "python": "",
-        "java": "",
+        "java": "/* Work in progress - Coming soon */"
     },
     "Authenticate": {
         "javascript": "",
-        "csharp": "",
-        "python": "",
-        "java": "",
+        "csharp": `API api = new API("user@example.com", "password", null, "database");
+await api.AuthenticateAsync();`,
+        "python": `api = mygeotab.API(username='user@example.com', password='password', database='database')
+api.authenticate()`,
+        "java": "/* Work in progress - Coming soon */"
     },
     "SetUserPassword": {
         "javascript": "",
         "csharp": "",
         "python": "",
-        "java": "",
+        "java": "/* Work in progress - Coming soon */"
     },
     "GenerateCaptcha": {
         "javascript": "",
         "csharp": "",
         "python": "",
-        "java": "",
+        "java": "/* Work in progress - Coming soon */"
     },
 
 
@@ -39,14 +61,55 @@ export const methodsCodeSamples: Record<string, CodeSamples> = {
 // "
     "Get": {
         "javascript": "",
-        "csharp": "await api.CallAsync<List<Device>>(\"Get\", typeof(Device), new { resultsLimit = 10 });",
-        "python": "api.get('Device', resultsLimit=10)",
-        "java": "",
+        "csharp": `await api.CallAsync<List<Device>>(
+    "Get",
+    typeof(Device),
+    new
+    {
+        resultsLimit = 10,
+        search = new DeviceSearch { FromDate = DateTime.UtcNow },
+        propertySelector = new PropertySelector
+        {
+            Fields = new List<string>
+            {
+                nameof(Device.Id),
+                nameof(Device.Name)
+            },
+            IsIncluded = true
+        }
+    }
+);`,
+        "python": `api.call_async(
+    "Get",
+    typeName = "Device",
+    resultsLimit = 10,
+    search = { "fromDate": datetime.datetime.now() },
+    propertySelector = { "fields": ["id", "name"] }
+)
+# OR
+api.get_async(
+    "Device",
+    resultsLimit = 10,
+    search = { "fromDate": datetime.datetime.now() },
+    propertySelector = { "fields": ["id", "name"] }
+)`,
+        "java": "/* Work in progress - Coming soon */"
     },
     "GetAddresses": {
         "javascript": "",
-        "csharp": "",
-        "python": "",
-        "java": "",
+        "csharp": `await api.CallAsync<List<ReverseGeocodeAddress>>(
+    "GetAddresses",
+    new
+    {
+        coordinates = 10,
+        movingAddresses = true
+    }
+);`,
+        "python": `api.call_async(
+    "GetAddresses",
+    coordinates = [{"x":"25.856667", "y":" -17.924444"}],
+    movingAddresses = True
+)`,
+        "java": "/* Work in progress - Coming soon */"
     }
 };
